faaf8032e01653bd24e70dd9fea679d0069178c8
Hung Pham <2411.phamquochung@gmail.com>
2015-05-04 10:57:18 +0700

    update home view

--- admin/home.php
+++ admin/home.php
@@ -9,9 +9,9 @@ include_once('../libraries/configs.php');
 
 <html>
     <head>
-<meta charset="utf-8"/>
+        <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width" />
-        <title>Hệ thống quản trị bdschinhchu.vn</title>
+        <title>Hệ thống quản trị</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
         <link href="css/Page_Head_Admin.css" rel="stylesheet" />
@@ -44,9 +44,6 @@ include_once('../libraries/configs.php');
                 var acc_head = $('.accordion_menu > li > a'),
                         acc_body = $('.accordion_menu li > .sub-menu');
 
-                // Mở menu tab đầu tiên khi load trang web
-                acc_head.first().addClass('active').next().slideDown('normal');
-
                 // Tạo sự kiện click cho các thẻ a trong tab
                 acc_head.on('click', function (event) {
 
@@ -66,13 +63,13 @@ include_once('../libraries/configs.php');
     </head>
     <body>
         <div class="Page_Head_Admin">
-            <img src="../Admin/images/banner.png" alt="" width="100%"  height="100"/>
+            <img src="images/banner.png" alt="" width="100%"  height="100"/>
         </div>
         <div class="Page_Content_Admin">
             <div class="Content_Admin_Left">
                 <ul class="accordion_menu">
-                    <li>
-                        <a href="#" class="active">Xin Chào :
+                    <li class="menu-item">
+                        <a class="menu-item-link" href="#">Xin Chào :
 
                             <?php
                             $r = $pdo->query("SELECT * FROM admins WHERE ad_Username='" . $_SESSION['login_user'] . "'");
@@ -82,53 +79,53 @@ include_once('../libraries/configs.php');
                             ?>
 
                         </a>
-                        <ul class="sub-menu" style="display: block; ">
-                            <li><a href="quanly.php?page=my-informations"><em>01</em>Thông tin cá nhân</a></li>
-                            <li><a href="quanly.php?page=edit-my-password"><em>02</em>Đổi mật khẩu</a></li>
-                            <li><a href="<?php echo PATH ?>" target="_blank"><em>03</em>Trang chủ</a></li>
-                            <li><a href="logout.php"><em>04</em>Thoát</a></li>
+                        <ul class="sub-menu">
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=my-informations"><em>01</em>Thông tin cá nhân</a></li>
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=edit-my-password"><em>02</em>Đổi mật khẩu</a></li>
+                            <li class="menu-item"><a class="menu-item-link" href="<?php echo PATH ?>" target="_blank"><em>03</em>Trang chủ</a></li>
+                            <li class="menu-item"><a class="menu-item-link" href="logout.php"><em>04</em>Thoát</a></li>
                         </ul>
                     </li>
-                    <?php
-                    if ($_SESSION['position'] == "2"): ?>
-                       
-							<li><a href="#" class="">I. Quản lý quản trị viên</a>
-								<ul class="sub-menu" style="display: none; ">
-									<li><a href="quanly.php?page=staff"><em>01</em>Danh sách quản trị viên</a></li>
-									<li><a href="quanly.php?page=add_staff"><em>02</em>Thêm quản trị viên mới</a></li>
-								</ul>
-							</li>
-                    
-                    <?php endif; ?> 
-                    <li><a href="#" class="">II. Tiêu đề và Dịch vụ</a>
-                        <ul class="sub-menu" style="display: none; ">
-                            <li><a href="quanly.php?page=categories"><em>01</em>Tiêu đề</a></li>
-                            <li><a href="quanly.php?page=add_category"><em>02</em>Dịch vụ</a></li>
+                    <?php if ($_SESSION['position'] == "2"): ?>
+
+                        <li class="menu-item"><a class="menu-item-link" href="#">I. Quản lý quản trị viên</a>
+                            <ul class="sub-menu">
+                                <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=staff"><em>01</em>Danh sách quản trị viên</a></li>
+                                <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=add_staff"><em>02</em>Thêm quản trị viên mới</a></li>
+                            </ul>
+                        </li>
+
+                    <?php endif; ?>
+                    <li class="menu-item"><a class="menu-item-link" href="#">II. Tiêu đề và Dịch vụ</a>
+                        <ul class="sub-menu">
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=categories"><em>01</em>Tiêu đề</a></li>
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=add_category"><em>02</em>Dịch vụ</a></li>
                         </ul>
                     </li>
-                    <li><a href="#" class="">III. Blog</a>
-                        <ul class="sub-menu" style="display: none; ">
-                            <li><a href="quanly.php?page=blog_categories"><em>01</em>Danh mục</a></li>
-                            <li><a href="quanly.php?page=blog"><em>02</em>Bài viết</a></li>
-                            <li><a href="quanly.php?page=tags"><em>03</em>Tags</a></li>
+                    <li class="menu-item"><a class="menu-item-link" href="#">III. Blog</a>
+                        <ul class="sub-menu">
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=blog_categories"><em>01</em>Danh mục</a></li>
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=blog"><em>02</em>Bài viết</a></li>
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=tags"><em>03</em>Tags</a></li>
                         </ul>
                     </li>
-					<li><a href="#" class="">IV. Nhân vật quan trọng</a>
-                        <ul class="sub-menu" style="display: none; ">
-                            <li><a href="quanly.php?page=vip"><em>01</em>Danh sách</a></li>
+                    <li class="menu-item"><a class="menu-item-link" href="#">IV. Nhân vật quan trọng</a>
+                        <ul class="sub-menu">
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=vip"><em>01</em>Danh sách</a></li>
                         </ul>
                     </li>
-                    <li><a href="#" class="">V. Khác</a>
-                        <ul class="sub-menu" style="display: none; ">
-                            <li><a href="quanly.php?page=options"><em>01</em>Thông tin chung</a></li>
-                            <li><a href="quanly.php?page=edit_slideshow"><em>02</em>Banner trang chủ</a></li>
-                            <li><a href="quanly.php?page=about_us"><em>03</em>Về chúng tôi</a></li>
-                            <li><a href="quanly.php?page=partners"><em>04</em>Các đối tác</a></li>
-                            <li><a href="quanly.php?page=feedback"><em>05</em>Các nhận xét</a></li>
-                            <li><a href="quanly.php?page=comments"><em>06</em>Liện hệ</a></li>
+                    <li class="menu-item"><a class="menu-item-link" href="#">V. Khác</a>
+                        <ul class="sub-menu">
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=options"><em>01</em>Thông tin chung</a></li>
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=edit_slideshow"><em>02</em>Banner trang chủ</a></li>
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=edit_banner_blog"><em>03</em>Banner trang Blog</a></li>
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=about_us"><em>04</em>Về chúng tôi</a></li>
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=partners"><em>05</em>Các đối tác</a></li>
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=feedback"><em>06</em>Các nhận xét</a></li>
+                            <li class="menu-item"><a class="menu-item-link" href="quanly.php?page=comments"><em>07</em>Liện hệ</a></li>
                         </ul>
                     </li>
-                    
+
                 </ul>
 
             </div>
--- admin/js/scriptmenu.js
+++ admin/js/scriptmenu.js
@@ -1,26 +1,87 @@
+/* get the value from the URL parameter
+ * JavaScript has nothing built in for handling query string parameters.
+ * We could access location.search,
+ * which would give you from the ? character on to the end of the fragment identifer (#foo),
+ * whichever came first.
+ */
+var QueryString = function () {
+};
+QueryString.prototype.getParameters = function (url) {
+    // This function is anonymous, is executed immediately and
+    // the return value is assigned to QueryString!
+    var query_string = {};
+
+    var query = url.split("?")[1];
+    if (typeof query === "undefined") {
+        console.log("URL don't have parameters\nURL: " + url);
+        return;
+    }
+
+    var vars = query.split("&");
+    if (typeof vars === "undefined") {
+        vars = query;
+    }
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        // If first entry with this name
+        if (typeof query_string[pair[0]] === "undefined") {
+            query_string[pair[0]] = pair[1];
+            // If second entry with this name
+        } else if (typeof query_string[pair[0]] === "string") {
+            var arr = [query_string[pair[0]], pair[1]];
+            query_string[pair[0]] = arr;
+            // If third or later entry with this name
+        } else {
+            query_string[pair[0]].push(pair[1]);
+        }
+    }
+    return query_string;
+};
+
+/*
+ * get array of menu and set menu active
+ */
+function activeMenu() {
+    var url = new QueryString();
+    //get parameters in address bar
+    var urlParameters = url.getParameters(window.location.href);
+
+    var menuItemLink = document.querySelectorAll(".accordion_menu .menu-item-link");
+    if (menuItemLink !== null) {
+        for (var i = 0; i < menuItemLink.length; i++) {
+            var parameters = url.getParameters(menuItemLink[i].getAttribute("href"));
+            if (typeof parameters !== "undefined" && urlParameters.page == parameters.page) {
+                menuItemLink[i].parentElement.parentElement.parentElement.classList.add("active");
+            }
+        }
+    }
+}
+
 // add event handler realization
 var addEvent = (function () {
-  if (document.addEventListener) {
-    return function (el, type, fn) {
-      if (el && el.nodeName || el === window) {
-        el.addEventListener(type, fn, false);
-      } else if (el && el.length) {
-        for (var i = 0; i < el.length; i++) {
-          addEvent(el[i], type, fn);
-        }
-      }
-    };
-  } else {
-    return function (el, type, fn) {
-      if (el && el.nodeName || el === window) {
-        el.attachEvent('on' + type, function () { return fn.call(el, window.event); });
-      } else if (el && el.length) {
-        for (var i = 0; i < el.length; i++) {
-          addEvent(el[i], type, fn);
-        }
-      }
-    };
-  }
+    if (document.addEventListener) {
+        return function (el, type, fn) {
+            if (el && el.nodeName || el === window) {
+                el.addEventListener(type, fn, false);
+            } else if (el && el.length) {
+                for (var i = 0; i < el.length; i++) {
+                    addEvent(el[i], type, fn);
+                }
+            }
+        };
+    } else {
+        return function (el, type, fn) {
+            if (el && el.nodeName || el === window) {
+                el.attachEvent('on' + type, function () {
+                    return fn.call(el, window.event);
+                });
+            } else if (el && el.length) {
+                for (var i = 0; i < el.length; i++) {
+                    addEvent(el[i], type, fn);
+                }
+            }
+        };
+    }
 })();
 
 // variables
@@ -37,7 +98,7 @@ addEvent(window, 'load', function (event) {
     aLoops[2] = new Audio('media/button_click.ogg');
     aLoops[2].volume = 1.0;
 
-    aLoops[0].addEventListener('ended', function() { // loop background sound
+    aLoops[0].addEventListener('ended', function () { // loop background sound
         this.currentTime = 0;
         this.play();
     }, false);
@@ -64,3 +125,11 @@ addEvent(aBtns, 'click', function (event) {
     aLoops[2].currentTime = 0;
     aLoops[2].play(); // play click sound
 });
+
+function addEventListeners() {
+    document.addEventListener("DOMContentLoaded", function () {
+        activeMenu();
+    });
+}
+
+addEventListeners();
